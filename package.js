3ff08a3258f426cfbe81808b8ca68d638718940d
Jacky Qiu <qunxyz@gmail.com>
2015-03-26 02:06:56 +0800

    updated git address

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "jackyqiu:meteor-jquery-nicescroll",
   summary: "Thank InuYaksa. Nicescroll is a jquery plugin, for nice customizabled scrollbars with a very similar ios/mobile style. It supports DIVs, IFrames and document page (body) scrollbars. Compatible with Firefox 4+, Chrome 5+, Safari 4+ (win/mac), Opera 10+, IE 6+ (all A-grade browsers). Compatible with iOS devices as iPad, Android, Blackberry, Windows Phone, and many many mobile and touch devices.",
   version: "3.6.0",
-  git: "https://github.com/inuyaksa/jquery.nicescroll.git"
+  git: "https://github.com/qunxyz/meteor-jquery-nicescroll.git"
 });
 
 Package.onUse(function (api) {
